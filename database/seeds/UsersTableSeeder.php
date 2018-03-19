0e647cad4a1caa8dd50196cfcdc81ad472eb1262
Welliton Klein <welliton.fokushima@gmail.com>
2018-03-19 17:19:25 -0300

    Criando login e logout para admins

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,9 +11,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(\CodeFin\User::class,1)->states('admin')->create([
+        factory(\CodeFin\User::class,1)
+            ->states('admin')->create([
             'name' => 'Fokushima San',
             'email' => 'admin@user.com'
         ]);
+
+        factory(\CodeFin\User::class,1)
+            ->create([
+                'name' => 'Joazin da Silva',
+                'email' => 'client@user.com'
+            ]);
     }
 }
