0d0f7954cd2424a3ce2d36a7a5a92c977ceb4221
Welliton Klein <welliton.fokushima@gmail.com>
2018-03-28 17:58:05 -0300

    Criando método para criar token

--- app/User.php
+++ app/User.php
@@ -4,8 +4,9 @@ namespace CodeFin;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -28,4 +29,32 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        // TODO: Implement getJWTIdentifier() method.
+        return $this->id;
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        // TODO: Implement getJWTCustomClaims() method.
+        return [
+            'user'=>[
+                'id'=>$this->id,
+                'name'=>$this->name,
+                'email'=>$this->email
+            ]
+        ];
+    }
 }
