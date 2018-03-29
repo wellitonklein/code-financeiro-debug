da0675f9ea1a27d1114c4ec34d146be4ac945aed
Welliton Klein <welliton.fokushima@gmail.com>
2018-03-29 16:43:50 -0300

    Tratamento de erros de login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,6 +7,7 @@ use CodeFin\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Lang;
 
 class AuthController extends Controller
 {
@@ -16,19 +17,54 @@ class AuthController extends Controller
     public function accessToken(Request $request){
         $this->validateLogin($request);
 
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
         $credentials = $this->credentials($request);
 
         if ($token = Auth::guard('api')->attempt($credentials)){
             //retornar o token
             return $this->sendLoginResponse($request,$token);
         }
+
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
     }
 
     protected function sendLoginResponse(Request $request, $token)
     {
+        $this->clearLoginAttempts($request);
+
         return response()->json([
             'token'=> $token
         ]);
+    }
+
+    public function sendLockoutResponse(Request $request)
+    {
+        $seconds = $this->limiter()->availableIn(
+            $this->throttleKey($request)
+        );
+
+        $message = Lang::get('auth.throttle',['seconds'=>$seconds]);
+
+        return response()->json([
+            'message'=>$message
+            ],403);
+    }
+
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        return response()->json([
+            'message'=> Lang::get('auth.failed')
+        ],401);
     }
 
     /**
