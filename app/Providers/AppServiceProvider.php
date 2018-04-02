6146d286e6b7c7b44df241141283b0776ff0d6aa
Welliton Klein <welliton.fokushima@gmail.com>
2018-04-02 18:45:05 -0300

    JWT Claims

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace CodeFin\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Tymon\JWTAuth\Manager;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton('tymon.jwt.manager', function ($app) {
+            $instance = new Manager(
+                $app['tymon.jwt.provider.jwt'],
+                $app['tymon.jwt.blacklist'],
+                $app['tymon.jwt.payload.factory']
+            );
+
+            return $instance->setBlacklistEnabled((bool) config('jwt.blacklist_enabled'));
+        });
     }
 }
