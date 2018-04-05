ba1e61ffcd9c9179828e06a9690db5c1634d8d78
Welliton Klein <welliton.fokushima@gmail.com>
2018-04-05 10:42:14 -0300

    Criação do serviço appConfig

--- resources/assets/spa/js/spa.js
+++ resources/assets/spa/js/spa.js
@@ -1,9 +1,11 @@
 import LocalStorage from './services/localStorage';
+import appConfig from './services/appConfig';
 
 require('materialize-css');
 window.Vue = require('vue');
 require('vue-resource');
-Vue.http.options.root = "http://0.0.0.0:9000/api";
+
+Vue.http.options.root = appConfig.api_url;
 
 require('./router');
 
