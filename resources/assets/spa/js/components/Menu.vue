9901072427d50217140c7b3a7664e76173ed23e0
Welliton Klein <welliton.fokushima@gmail.com>
2018-04-06 17:23:40 -0300

    Criação do menu interno

---
+++ resources/assets/spa/js/components/Menu.vue
@@ -0,0 +1,81 @@
+<template>
+    <ul :id="o.id" class="dropdown-content" v-for="o in menusDropDown">
+        <li v-for="item in o.items">
+            <a v-link="{name: item.routeName}">{{item.name}}</a>
+        </li>
+    </ul>
+    <ul id="dropdown-logout" class="dropdown-content">
+        <li>
+            <a v-link="{name: 'auth.logout'}">Sair</a>
+        </li>
+    </ul>
+    <div>
+        <div class="navbar-fixed">
+            <nav>
+                <div class="nav-wrapper container">
+                    <div class="col s12">
+                        <a href="#" class="left brand-logo">Contas <i class="material-icons">local_grocery_store</i></a>
+                        <a href="#" data-activates="nav-mobile" class="button-collapse">
+                            <i class="material-icons">menu</i>
+                        </a>
+                        <ul class="right hide-on-med-and-down">
+                            <li v-for="menu in menus">
+                                <a v-if="menu.dropDownID" class="dropdown-button" href="!#" :data-activates="menu.dropDownID">
+                                    {{menu.name}} <i class="material-icons right">arrow_drop_down</i>
+                                </a>
+                                <a v-else v-link="{name: menu.url}">{{menu.name}}</a>
+                            </li>
+                            <li>
+                                <a href="!#" class="dropdown-button" data-activates="dropdown-logout">
+                                    {{ name }} <i class="material-icons right">arrow_drop_down</i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <ul id="nav-mobile" class="side-nav">
+                        <li v-for="menu in menus">
+                            <a v-link="{name: menu.url}">{{menu.name}}</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<script type="text/javascript">
+    import Auth from '../services/auth';
+    export default {
+        data() {
+            return{
+                menus: [
+                    {name:"Contas a Pagar", dropDownID: 'teste'},
+                    {name:"Contas a Receber", routeName: 'auth.login'}
+                ],
+                menusDropDown:[
+                    {
+                        id: 'teste',
+                        items: [
+                            {name:"Listar Contas", routeName: 'auth.login'},
+                            {name:"Criar Conta", routeName: 'auth.login'}
+                        ]
+                    }
+                ],
+                user: Auth.user
+            }
+        },
+        computed:{
+            name(){
+                return this.user.data ? this.user.data.name : '';
+            }
+        },
+        ready(){
+            $('.button-collapse').sideNav();
+            $('.dropdown-button').dropdown();
+        }
+    };
+</script>
+
+<style type="text/css" scoped>
+
+</style>
--- resources/assets/spa/js/services/auth.js
+++ resources/assets/spa/js/services/auth.js
@@ -4,15 +4,37 @@ import {User} from '../services/resources';
 
 const USER = 'user';
 
-const afterLogin = (response) => {
+const afterLogin = function(response) {
+    this.user.check = true;
     User.get()
-        .then((response)=>LocalStorage.setObject(USER,response.data));
+        .then((response)=>{
+            this.user.data = response.data;
+        });
 };
 
 export default {
+    user:{
+        set data(value){
+            if (!value) {
+                LocalStorage.remove(USER);
+                this._data = null;
+                return;
+            }
+            this._data = value;
+            LocalStorage.setObject(User,value);
+        },
+        get data(){
+            if (!this._data){
+                this._data = LocalStorage.getObject(User);
+            }
+            return this._data;
+        },
+        check: JwtToken.token ? true : false
+    },
     login(email, password){
         return JwtToken.accessToken(email,password).then((response)=>{
-            afterLogin(response);
+            let afterLoginContext = afterLogin.bind(this);
+            afterLoginContext(response);
             return response;
         });
     },
@@ -21,17 +43,12 @@ export default {
             this.clearAuth();
         };
 
-        return JwtToken.logout()
+        return JwtToken.revokeToken()
             .then(afterLogout())
             .catch(afterLogout());
     },
-    user(){
-        return LocalStorage.getObject(USER);
-    },
-    check(){
-        return JwtToken.token ? true : false;
-    },
     clearAuth(){
-        LocalStorage.remove(USER);
+        this.user.data = null;
+        this.user.check = false;
     }
 }
