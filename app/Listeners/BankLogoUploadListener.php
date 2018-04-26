1875d9ac01c315cbaa9266d1bae63955c0e2267c
welliton <welliton.fokushima@gmail.com>
2018-04-26 12:39:49 -0300

    Criação - implementação do upload do logo

--- app/Listeners/BankLogoUploadListener.php
+++ app/Listeners/BankLogoUploadListener.php
@@ -3,19 +3,23 @@
 namespace CodeFin\Listeners;
 
 use CodeFin\Events\BankStoredEvent;
+use CodeFin\Models\Bank;
+use CodeFin\Repositories\BankRepository;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Storage;
 
 class BankLogoUploadListener
 {
+    private $repository;
     /**
      * Create the event listener.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(BankRepository $repository)
     {
-        //
+        $this->repository = $repository;
     }
 
     /**
@@ -27,5 +31,14 @@ class BankLogoUploadListener
     public function handle(BankStoredEvent $event)
     {
         //upload do logo
+        $bank = $event->getBank();
+        $logo = $event->getLogo();
+
+        $name = md5(time()).'.'.$logo->guessExtension();
+        $destFile = Bank::logosDir();
+
+        Storage::disk('public')->putFileAs($destFile,$logo,$name);
+
+        $this->repository->update(['logo' => $name],$bank->id);
     }
 }
