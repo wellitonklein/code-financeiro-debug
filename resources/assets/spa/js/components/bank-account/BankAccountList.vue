1b0f68d9574ff0e105f6a282e9e9e805f745c270
welliton <welliton.fokushima@gmail.com>
2018-04-30 17:06:17 -0300

    Criando componente de paginação

--- resources/assets/spa/js/components/bank-account/BankAccountList.vue
+++ resources/assets/spa/js/components/bank-account/BankAccountList.vue
@@ -34,6 +34,7 @@
                     </tr>
                     </tbody>
                 </table>
+                <pagination :per-page="10" :total-records="55"></pagination>
             </div>
 
             <div class="fixed-action-btn">
@@ -68,10 +69,12 @@
 <script>
     import {BankAccount} from "../../services/resources";
     import ModalComponent from "../../../../_default/components/Modal.vue";
+    import PaginationComponent from "../Pagination.vue";
 
     export default {
         component: {
             'modal': ModalComponent,
+            'pagination' : PaginationComponent
         },
         data(){
             return{
